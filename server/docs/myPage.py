57b92c2503f895319b321d1569d7e60c8705e040
by09115 <by09115@outlook.kr>
2018-12-24 11:11:01 +0900

    |create| docs 중 마이페이지 관련 내용 추가

--- server/docs/myPage.py
+++ server/docs/myPage.py
@@ -1,5 +1,29 @@
 from . import param
 
+MY_PAGE_POST = {
+    'tags': ['my-page'],
+    'parameters': [
+        {
+            'name': 'Authorization',
+            'description': "헤더로 jwt 받아오기",
+            'in': 'header',
+            'type': 'str',
+            'required': True
+        },
+        param('profile-image', "프로필 이미지", required=False),
+        param('background-imgae', "(프로필)배경 이미지", required=False),
+        param('introduction', '한줄 자기소개', required=False),
+    ],
+    'responses': {
+        '201': {
+            'description': "마이페이지 정보 수정 성공",
+        },
+        '403': {
+            'description': "JWT 인증 실"
+        }
+    }
+}
+
 MY_PAGE_GET = {
     'tags': ['my-page'],
     'parameters': [
