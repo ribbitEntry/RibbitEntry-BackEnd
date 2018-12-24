5abbd547e761faf105934aa94ac04f6a5fd491e0
Gaegul <eunjuoi0515@gmail.com>
2018-12-24 11:23:09 +0900

    [CREATE] post api

---
+++ server/view/post.py
@@ -0,0 +1,15 @@
+from flask_restful import Resource
+from flasgger import swag_from
+
+from docs.posts import POST_POST, POST_DELETE
+
+
+class Post(Resource):
+
+    @swag_from(POST_POST)
+    def post(self):
+        pass
+
+    @swag_from(POST_DELETE)
+    def delete(self):
+        pass
