52e2b36ec92c0e187950df64e3b98642d447e785
Gaegul <eunjuoi0515@gmail.com>
2018-12-24 11:04:34 +0900

    [CREATE] POST SWAGGER

---
+++ server/docs/posts.py
@@ -0,0 +1,30 @@
+from . import param
+
+POST_POST = {
+    'tags': ['post'],
+    'parameters': [
+        param('title', "제목"),
+        param('content', "내용"),
+        param('image', "이미지")
+    ],
+    'responses': {
+        '201': {
+            'description': "글 작성 성공",
+        },
+        '400': {
+            'description': "제목, 내용을 모두 채워주세요."
+        }
+    }
+}
+
+POST_DELETE = {
+    'tags': ['post'],
+    'parameters': [
+        param('post_id', "제목")
+    ],
+    'responses': {
+        '200': {
+            'description': "글 삭제 성공",
+        }
+    }
+}
