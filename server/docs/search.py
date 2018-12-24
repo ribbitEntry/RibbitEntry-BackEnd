ecfd6d3b35ead413a4d2f657ef512d01b1bcb70f
Gaegul <eunjuoi0515@gmail.com>
2018-12-24 11:04:52 +0900

    [CREATE] SEARCH SWAGGER

---
+++ server/docs/search.py
@@ -0,0 +1,37 @@
+from . import param
+
+POST_POST = {
+    'tags': ['search'],
+    'parameters': [
+        param('search_word', "검색어"),
+    ],
+    'responses': {
+        '200': {
+            'description': "검색시작",
+        },
+        '400': {
+            'description': "검색어를 입력하세요."
+        }
+    }
+}
+
+POST_GET = {
+    'tags': ['search'],
+    'parameters': [
+        param('search_word', "검색어"),
+    ],
+    'responses': {
+        '200': {
+            'description': "검색완료.",
+            "example": {
+                "nickname": "닉네임",
+                "id": "아이디",
+                "proimg": "프로필사진",
+                "title": "글 제목"
+            }
+        },
+        '404': {
+            'description': "없는 검색어입니다."
+        }
+    }
+}
