e6d9bee0639f8d2fa6f3626f044ea5722fe0b954
by09115 <by09115@outlook.kr>
2018-12-31 14:13:14 +0900

    |edit| os.environ -> os.getenv()

--- server/view/user/login/__init__.py
+++ server/view/user/login/__init__.py
@@ -4,8 +4,8 @@ import requests
 
 def validate_email(email):
     responsed = requests.get(
-        url=str(os.environ['EMAIL_VALIDATE_URL']),
-        auth=("api", str(os.environ['EMAIL_VALIDATE_KEY'])),
+        url=str(os.getenv('EMAIL_VALIDATE_URL')),
+        auth=("api", str(os.getenv('EMAIL_VALIDATE_KEY'))),
         params={"address": str(email)}
     ).json()
     return dict(responsed)['is_valid']
--- server/view/user/login/login.py
+++ server/view/user/login/login.py
@@ -1,4 +1,4 @@
-from flask import request
+from flask import request, Response
 from flasgger import swag_from
 from flask_restful import Resource
 from flask_jwt_extended import create_access_token, create_refresh_token
@@ -17,11 +17,16 @@ class Login(Resource):
         password = payload['password']
 
         if userId and password:
-            if validate_email(email=userId) and User.query.filter(User.id == userId).first():
-                return {
-                    'access_token': create_access_token(identity=userId),
-                    'refresh_token': create_refresh_token(identity=userId),
-                    'color_set': 'processing'
-                }, 200
+            if validate_email(email=userId):
+                if User.query.filter(User.id == userId).first():
+                    return {
+                        'access_token': create_access_token(identity=userId),
+                        'refresh_token': create_refresh_token(identity=userId),
+                        'color_set': 'processing'
+                    }, 200
+                else:
+                    return Response("없는 계정이거나 잘못된 비밀번호")
             else:
-                return {"status": "incorrectly authentication info"}, 401
+                return Response("유효하지 않은 이메일"), 405
+        else:
+            return {"status": "incorrectly authentication info"}, 401
