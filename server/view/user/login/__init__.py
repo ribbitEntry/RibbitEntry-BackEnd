82f5cd4453b10a20b668b3bc811c9eebb315e9df
by09115 <by09115@outlook.kr>
2018-12-31 11:53:20 +0900

    |edit| os.environ 적용

--- server/view/user/login/__init__.py
+++ server/view/user/login/__init__.py
@@ -1,12 +1,11 @@
+import os
 import requests
-
-from os import getenv
 
 
 def validate_email(email):
     responsed = requests.get(
-        getenv('EMAIL_VALIDATE_URL'),
-        auth=("api", getenv('EMAIL_VALIDATE_KEY')),
+        url=str(os.environ['EMAIL_VALIDATE_URL']),
+        auth=("api", str(os.environ['EMAIL_VALIDATE_KEY'])),
         params={"address": str(email)}
     ).json()
     return dict(responsed)['is_valid']
--- server/view/user/login/login.py
+++ server/view/user/login/login.py
@@ -17,7 +17,7 @@ class Login(Resource):
         password = payload['password']
 
         if userId and password:
-            if validate_email(userId) and User.query.filter(User.id == userId).first():
+            if validate_email(email=userId) and User.query.filter(User.id == userId).first():
                 return {
                     'access_token': create_access_token(identity=userId),
                     'refresh_token': create_refresh_token(identity=userId),
