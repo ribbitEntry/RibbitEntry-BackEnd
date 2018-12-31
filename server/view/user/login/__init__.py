4ce02ac968e21310b51eb89fbf4f350d6c26a518
by09115 <by09115@outlook.kr>
2018-12-31 14:34:06 +0900

    |edit| request.get 사용 안함

--- server/view/user/login/__init__.py
+++ server/view/user/login/__init__.py
@@ -3,9 +3,9 @@ import requests
 
 
 def validate_email(email):
-    responsed = requests.get(
-        url=str(os.getenv('EMAIL_VALIDATE_URL')),
-        auth=("api", str(os.getenv('EMAIL_VALIDATE_KEY'))),
-        params={"address": str(email)}
-    ).json()
+    responsed = requests.request(method="GET",
+                                 url=str(os.getenv('EMAIL_VALIDATE_URL')),
+                                 auth=("api", str(os.getenv('EMAIL_VALIDATE_KEY'))),
+                                 params={"address": str(email)}
+                                 ).json()
     return dict(responsed)['is_valid']
