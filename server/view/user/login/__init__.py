028b9f44b753f54271dad03c40c608076527532f
by09115 <by09115@outlook.kr>
2018-12-31 16:40:15 +0900

    |edit| 왜 환경변수를 못불러오니 ㅠㅠ

--- server/view/user/login/__init__.py
+++ server/view/user/login/__init__.py
@@ -3,9 +3,9 @@ import requests
 
 
 def validate_email(email):
-    responsed = requests.request(method="GET",
-                                 url="https://api.mailgun.net/v3/address/validate",
-                                 auth=("api", str(os.getenv('EMAIL_VALIDATE_KEY'))),
-                                 params={"address": str(email)}
-                                 ).json()
+    responsed = requests.get(
+        url="https://api.mailgun.net/v3/address/validate",
+        auth=("api", str(os.getenv('EMAIL_VALIDATE_KEY'))),
+        params={"address": str(email)}
+    ).json()
     return dict(responsed)['is_valid']
