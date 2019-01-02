fc6d3f128b662567803a344699ec15a2686b2483
by09115 <by09115@outlook.kr>
2019-01-02 11:05:55 +0900

    | edit | 조건문 예외처리

--- server/view/user/myPage/MyPage.py
+++ server/view/user/myPage/MyPage.py
@@ -1,9 +1,10 @@
 # from database import db_session
+from
 from flasgger import swag_from
 from flask_restful import Resource
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
-from server.docs.myPage import MY_PAGE_GET
+from server.docs.myPage import MY_PAGE_GET, MY_PAGE_POST
 from server.model.user import User
 from server.model.post import Post
 from server.model.comment import Comment
@@ -17,31 +18,44 @@ class MyPage(Resource):
         user_id = get_jwt_identity()
         user_info = User.query.filter(User.id == user_id).first()
         post_info = Post.query.filter(user=user_id).all()
-        if user_info and post_info:
+
+        user_info_ = {
+            "nickname": user_info.nickname,
+            "profile_image": user_info.proimg,
+            "background_image": user_info.backimg,
+            "introduction": user_info.introduction,
+            "follow_num": user_info.follow_num,
+            "follower_num": user_info.follower_num
+        }
+
+        if not user_info:
+            return {"status": "invalid authentication"}, 401
+
+        elif user_info and not post_info:
+            return {"user_info": user_info_}, 200
+
+        elif user_info and post_info:
             return {
-                "user_info": {
-                    "nickname": user_info.nickname,
-                    "profile_image": user_info.proimg,
-                    "background_image": user_info.backimg,
-                    "introduction": user_info.introduction,
-                    "follow_num": user_info.follow_num,
-                    "follower_num": user_info.follower_num
-                },
-                "post": [
-                    {
-                        "title": post.title,
-                        "content": post.content,
-                        "image": post.image,
-                        "user": post.user,
-                        "date": post.date,
-                        "like": post.like,
-                        "comment": [
-                            {
-                                "title": comment.title,
-                                "content": comment.content,
-                                "date": comment.date
-                            } for comment in Comment.query.filter()]
-                    } for post in post_info]
-            }
-        else:
-            pass
+                       "user_info": user_info_,
+                       "post": [
+                           {
+                               "title": posts.title,
+                               "content": posts.content,
+                               "image": posts.image,
+                               "user": posts.user,
+                               "date": posts.date,
+                               "like": posts.like,
+                               "comment": [
+                                   {
+                                       "comment_id": Comment.query.filter(comment_id=comment).first().comment_id,
+                                       "user": Comment.query.filter(comment_id=comment).first().user,
+                                       "title": Comment.query.filter(comment_id=comment).first().title,
+                                       "content": Comment.query.filter(comment_id=comment).first().content,
+                                       "date": Comment.query.filter(comment_id=comment).first().date
+                                   } for comment in posts.comment]
+                           } for posts in post_info]
+                   }, 200
+
+    @swag_from(MY_PAGE_POST)
+    def post(self):
+        pass
