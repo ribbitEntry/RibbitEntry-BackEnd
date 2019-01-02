a86054e81dae87461b3c36f7b8036fb0fcc84c25
Gaegul <eunjuoi0515@gmail.com>
2019-01-02 12:14:54 +0900

    [DELETE] POST search 기능 없이 유저검색만 구현

--- server/view/search/search.py
+++ server/view/search/search.py
@@ -4,7 +4,6 @@ from flask import request
 
 from server.docs.search import SEARCH_POST
 from server.model.user import User
-from server.model.post import Post
 
 
 class Search(Resource):
@@ -12,35 +11,13 @@ class Search(Resource):
     @swag_from(SEARCH_POST)
     def post(self):
         search_word = request.json['search_word']
-        user_info = User.query.filter(search_word == User.id)
-        post_info = User.query.filter(search_word == Post.title)
-
-        user_info_ = {
-            "nickname": user_info.nickname,
-            "profile_image": user_info.proimg,
-            "introduction": user_info.introduction,
-            "follow_num": user_info.follow_num,
-            "follower_num": user_info.follower_num
-        }
-
-        post_info_ = {
-            "title": post_info.title,
-            "user": post_info.user,
-            "date": post_info.date
-        }
+        user_info = User.query.filter(search_word == User.nickname)
 
         if user_info:
-
-            if post_info:
-                return {
-                    "user_info": user_info_,
-                    "post_info": post_info_
-                }
-
-            else:
-                return {
-                    "user_info": user_info_
-                }
+            return {
+                "nickname": user_info.nickname,
+                "profile_image": user_info.proimg
+            }, 200
 
         else:
-            return {"status": "없는 검색어입니다."}, 404
+            return {"status": "없는 유저입니다."}, 404
