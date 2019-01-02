38549d9ae671201d2a84cf302d332e261ea2a817
Gaegul <eunjuoi0515@gmail.com>
2019-01-02 11:13:44 +0900

    [FIX] return값 바꿈

--- server/view/posts/post.py
+++ server/view/posts/post.py
@@ -21,12 +21,9 @@ class Posts(Resource):
             post = Post(title=title, content=content, image=image, user=get_jwt_identity())
             db.session.add(post)
             db.session.commit()
-            return '글 작성 완료', 201
+            return {'status': '글 작성 완료'}, 201
         else:
-            return '제목과 글 모두 작성해주세요', 400
-
-        
-
+            return {'status': '제목과 글 모두 작성해주세요'}, 400
 
     @swag_from(POST_DELETE)
     def delete(self):
