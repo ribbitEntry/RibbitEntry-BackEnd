8c718aada707e0d66c280efab8a343f36eff4dca
Gaegul <eunjuoi0515@gmail.com>
2019-01-03 11:13:03 +0900

    [CHANGE] return 값을 변경

--- server/view/color/color.py
+++ server/view/color/color.py
@@ -18,6 +18,6 @@ class Color(Resource):
             user = User.query.filter(User.id == user_id).first()
             user.theme_color = color
             db.session.commit()
-            return user.theme_color, 200
+            return {'color': user.theme_color}, 200
         else:
             return unicode_safe_json_dumps({'status': '컬러가 없습니다.'}, 400)
