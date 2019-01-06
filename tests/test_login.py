819e26e44b17e501cbd880ffe1b975a2defc7ca1
by09115 <by09115@outlook.kr>
2019-01-06 16:18:01 +0900

    | create | 로그인(로그인, 회원가입 등) 테스트 코드 작성

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,8 +1,26 @@
-from tests import TCBase, check_status_code
+from . import TCBase, check_status_code
 
 
 class LoginTest(TCBase):
 
-    def setUp(self):
-        super(LoginTest, self).setUp()
-        self.short_url = self.save_url_request()
+    def register(self, userId, pw, nickname):
+        return self.app.post(
+            '/api/sign-up',
+            data=dict(userId=userId, password=pw, nickname=nickname),
+            follow_redirects=True
+        )
+
+    def login(self, userId, pw):
+        return self.app.post(
+            '/api/login',
+            data=dict(userId=userId, password=pw)
+        )
+
+    def test_valid_registration(self):
+        response = self.register('onlyfortest@gmail.com', 'thisispassword', 'mynickname')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('Thank you for sign in our service!', response.data)
+
+    def test_invalid_registration_non_nickname(self):
+        response = self.register('onlyfortest@gmail.com', 'thisispassword')
+        self.assertIn("nickname hasn't found.", response.data)
