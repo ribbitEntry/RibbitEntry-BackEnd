172f7ba3962bf11066f4e1060a8080bdaa8fee73
by09115 <by09115@outlook.kr>
2019-01-23 08:25:23 +0900

    [UPDATE] 서버를 실행할 때 적용할 Config 선택 가능

--- runServer.py
+++ runServer.py
@@ -1,6 +1,11 @@
+import os
+
 from server import create_app
 
-app = create_app()
-
 if __name__ == '__main__':
+    app = create_app()
+
+    if 'SECRET_KEY' not in os.environ:
+        print("[WARNING] SECRET KEY NOT FOUNDED IN ENVIRONMENT VARIABLE")
+
     app.run('0.0.0.0', port=5000, debug=True)
